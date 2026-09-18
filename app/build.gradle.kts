855c9fb8d0be860cdfb1373da7c56067b01bacdd
imuer <song.lee.vip@outlook.com>
2026-09-19 00:51:18 +0800

    #24 更新版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 3
-        versionName = "2.3.0"
+        versionName = "2.3.1"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a")
